ec1cc4012bc1be6785be08f3d2325ae979c9f105
sam5987r <sam5987rodriquez@outlook.com>
2024-08-22 10:21:34 +0530

    Create src_components_Receipt.js

---
+++ src_components_Receipt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h2>Receipt</h2>
+      <p>Thank you for your purchase!</p>
+      <p>Amount: $35.00</p>
+      <p>Date: 2024-08-10</p>
+    </div>
+  );
+}
+
+export default Receipt;
